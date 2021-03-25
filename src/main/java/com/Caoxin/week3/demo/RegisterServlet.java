0f1dd123753b8039583a7ad1ef0ac320e2cbb30b
2019211001001117-Caoxin <2762101126@qq.com>
2021-03-25 22:01:23 +0800

    HW-week 3- commit

---
+++ src/main/java/com/Caoxin/week3/demo/RegisterServlet.java
@@ -0,0 +1,32 @@
+package com.Caoxin.week3.demo;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+//automatic -new --> servlet
+public class RegisterServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//request come here- <from method=post>
+        //get parameter from request .
+       String username = request.getParameter("username");//name of input type-<input type="text" name="username"/>
+       String password = request.getParameter("password");//<input type="password" name="email">
+       String email = request.getParameter("email");//<input type="text" name="email"/>
+       String gender = request.getParameter("gender");//<input type="radio" name="gender">
+       String birthDate = request.getParameter("birthDate");//<input type="text name=" name="birthDate">
+       //print - write into request
+        PrintWriter writer = response.getWriter();
+        writer.println("<br> username :"+username);
+        writer.println("<br> password :"+password);
+        writer.println("<br> email :"+email);
+        writer.println("<br> gender :"+gender);
+        writer.println("<br> birthDate ："+birthDate);
+        writer.close();
+    }
+}
---
+++ src/main/webapp/register.jsp
@@ -0,0 +1,27 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: CAOX
+  Date: 2021/3/25
+  Time: 19:56
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+
+<form method="post" action="register"><!-- whthin doPost() in Servlet-->
+    usernbame<input type="text" name="username"/><br/>
+    password<input type="password" name="email"><br/>
+    Email<input type="text" name="email"/><br/>
+    Gender:<input type="radio" name="gender" value="Male">Male <input type="radio" name="gender" value="Female">Female<br/>
+    <!-- if name is same it makes array-->
+    Date of Birth :<input type="text name=" name="birthDate"><br/>
+    <input type="submit" value="Register"/>
+
+</form>
+
+</body>
+</html>
